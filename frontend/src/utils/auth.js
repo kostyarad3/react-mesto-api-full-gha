2632b83efb6a4a887d34bb709688f55321a1f892
Konstantin Radchenko <kostya.rad3@yandex.ru>
2023-06-30 11:59:12 +0300

    fix: fixed url

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,47 +1,46 @@
-export const BASE_URL = 'https://api.mesto.kostyarad.nomoreparties.sbs/';
+export const BASE_URL = "https://api.mesto.kostyarad.nomoreparties.sbs";
 
-function getResponseData (res) {
+function getResponseData(res) {
   if (res.ok) {
     return res.json();
   }
   return Promise.reject(`Где-то ошибочка:( : ${res.status}`);
 }
 
-export function register (email, password) {
+export function register(email, password) {
   return fetch(`${BASE_URL}/signup`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({email, password})
-  })
-  .then(res => getResponseData(res))
-};
+    body: JSON.stringify({ email, password }),
+  }).then((res) => getResponseData(res));
+}
 
 export const login = (email, password) => {
   return fetch(`${BASE_URL}/signin`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({email, password})
+    body: JSON.stringify({ email, password }),
   })
-  .then((res => getResponseData(res)))
-  .then((data) => {
-    if (data.token){
-      localStorage.setItem('jwt', data.token);
-      return data;
-    }
-  })
-}; 
+    .then((res) => getResponseData(res))
+    .then((data) => {
+      if (data.token) {
+        localStorage.setItem("jwt", data.token);
+        return data;
+      }
+    });
+};
 
 export function getContent(jwt) {
   return fetch(`${BASE_URL}/users/me`, {
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${jwt}`
-    }
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt}`,
+    },
   })
-  .then(res => getResponseData(res))
-  .then(data => data)
-};
+    .then((res) => getResponseData(res))
+    .then((data) => data);
+}
