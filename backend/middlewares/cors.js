0d45c6ff25d678bfe5a14c5c8219deb5c3442bf3
Konstantin Radchenko <kostya.rad3@yandex.ru>
2023-07-04 22:24:24 +0300

    fix 3

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const allowedCors = [
   'https://api.mesto.kostyarad.nomoreparties.sbs',
   'https://mesto.kostyarad.nomoreparties.sbs',
@@ -11,16 +12,16 @@ const cors = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
   }
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
+  res.header('Access-Control-Allow-Credentials', true);
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
+    return res.end();
   }
 
   return next();
